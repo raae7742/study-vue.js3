7e800b990d53d2d11ebbebbe0fd3273fa6c61fb7
Hyeonae Jang <aeae_1@naver.com>
2022-03-03 15:04:57 +0900

    Props, $emit, event bus, mixins

---
+++ props-test/props 예제코드/3_뚡뀸_롟뀫__め꼳___롟뀶_뚡뀳_솽꼪_⒰넫/src/components/User.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="blue lighten-3 pa-3">
+    <h1>User 컴포넌트</h1>
+    <p>이름: {{ name }}</p>
+    <p>{{ getDateAndTime(createdAt) }}</p>
+    <hr>
+    <v-layout row wrap>
+      <v-flex xs12 sm6>
+        <UserDetail 
+          :name="name"
+          :address="address"
+          :phone="phone"
+          :hasDog="hasDog"
+        ></UserDetail>
+      </v-flex>
+      <v-flex xs12 sm6>
+        <UserEdit
+          :name="name"
+          :address="address"
+          :phone="phone"
+          :hasDog="hasDog"
+          @child="parents"
+        ></UserEdit>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import UserDetail from "./UserDetail.vue"
+import UserEdit from "./UserEdit.vue"
+import { dateFormat } from "../mixins/dateFormat.js"
+
+export default {
+  components: {
+    UserDetail,
+    UserEdit
+  },
+  data () {
+    return {
+      name: 'Hoza',
+      address: 'Seoul',
+      phone: '1234-5678',
+      hasDog: true,
+      createdAt: null,
+    }
+  },
+  created() {
+    this.createdAt = new Date()
+  },
+  methods: {
+    parents(user) {
+      this.name = user.name
+      this.address = user.address
+      this.phone = user.phone
+      this.hasDog = user.hasDog
+      console.log('부모가 받았어!')
+    },
+  },
+  mixins: [dateFormat]
+}
+</script>
