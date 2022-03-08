0803e23665edc6450d7105398bb3748eb746a7c6
hyeonae Jang <aeae_1@naver.com>
2022-03-08 15:34:29 +0900

    Vue-router: child

--- vue-router/vue-router/src/router.js
+++ vue-router/vue-router/src/router.js
@@ -5,8 +5,9 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 const About = () => import(/* webpackChunkName: "about" */ './views/About.vue')
-
 const Users = () => import('./views/Users.vue')
+const UsersDetail = () => import('./views/UsersDetail.vue')
+const UsersEdit = () => import('./views/UsersEdit.vue')
 
 export default new Router({
   mode: 'history',
@@ -20,18 +21,24 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: About
     },
     {
       path: '/users',
       name: 'users',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: Users
-    }
+      component: Users,
+      children:[
+        {
+          path: ':id',
+          name: 'users-detail',
+          component: UsersDetail,
+        },
+        {
+          path: ':id/edit',
+          name: 'users-edit',
+          component: UsersEdit,
+        }
+      ]
+    },
   ]
 })
---
+++ vue-router/vue-router/src/views/UsersEdit.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Users Edit</h1>
+        {{ userId }}
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        userId() {
+            return this.$route.params.id
+        }
+    },
+    methods: {}
+}
+</script>
