f3cabadfe0ed94ffb7f846ec3396bb59ac260ca1
hyeonae Jang <aeae_1@naver.com>
2022-03-10 15:04:49 +0900

    Vuex-login: 아이디&비밀번호 확인

--- ex-project/src/views/LoginView.vue
+++ ex-project/src/views/LoginView.vue
@@ -2,6 +2,18 @@
     <v-container fill-height style="max-width:350px">
         <v-layout align-center row wrap>
             <v-flex xs12>
+                    <v-alert
+                        class="mb-3"
+                        :value="isError"
+                        type="error" >
+                        아이디와 비밀번호를 확인해주세요.
+                    </v-alert>
+                    <v-alert
+                        class="mb-3"
+                        :value="isSuccess"
+                        type="success" >
+                        로그인이 완료되었습니다.
+                    </v-alert>
                 <v-card>
                     <v-toolbar flat>
                         <v-toolbar-title>로그인</v-toolbar-title>
@@ -16,6 +28,11 @@
                             type="password"
                             label="패스워드를 입력하세요.">
                         </v-text-field>
+                        <v-btn depressed large block
+                            color="primary"
+                            @click="login">
+                            로그인
+                        </v-btn>
                     </div>
                 </v-card>
             </v-flex>
@@ -28,7 +45,30 @@ export default {
     data() {
         return {
             email: null,
-            password: null
+            password: null,
+            isError: false,
+            isSuccess: false,
+            allUsers: [
+                { id:1, name:"hoza", email:"hoza@gmail.com", password:"123456" },
+                { id:1, name:"lego", email:"lego@gmail.com", password:"234567" },
+            ]
+        }
+    },
+    methods: {
+        login() {
+            // 전체 유저에서 이메일로 유저 찾기
+            let selectedUser = null
+            this.allUsers.forEach(user => {
+                if (user.email == this.email) selectedUser = user
+            })
+
+            selectedUser === null
+            ? (this.isError = true)
+            : selectedUser.password !== this.password
+                ? (this.isError = true)
+                : (this.isSuccess = true)
+            // 유저의 비밀번호와 입력한 비밀번호를 비교
+            
         }
     }
 }
